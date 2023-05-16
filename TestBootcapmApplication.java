fdd8eac6eede13be93a724a08f7ab098d11d816e
Abdulmajeed <132199458+mj95d@users.noreply.github.com>
2023-05-17 01:27:32 +0300

    TestBootcapmApplication.java

---
+++ TestBootcapmApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbootcapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBootcapmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBootcapmApplication.class, args);
+    }
+
+}
